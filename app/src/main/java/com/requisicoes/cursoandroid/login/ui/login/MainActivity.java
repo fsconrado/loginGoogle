ed6f37fcb896f46f735d5912b2cdfa15fe24f281
Conrado <fabiano@nti.ufal.br>
2019-06-25 09:32:53 -0300

    new Commit

--- app/src/main/java/com/requisicoes/cursoandroid/login/ui/login/MainActivity.java
+++ app/src/main/java/com/requisicoes/cursoandroid/login/ui/login/MainActivity.java
@@ -43,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
                             public void onComplete(@NonNull Task<Void> task) {
                                 btn_sign_out.setEnabled(false);
                                 showSignInOptions();
-
                             }
                         }).addOnFailureListener(new OnFailureListener() {
                     @Override
