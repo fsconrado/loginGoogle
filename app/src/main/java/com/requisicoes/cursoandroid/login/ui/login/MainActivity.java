bcbf9b90d304a40e8c248bdf8ddbc15b15c12ced
Conrado <fabiano@nti.ufal.br>
2019-06-25 09:26:17 -0300

    Initial commit

---
+++ app/src/main/java/com/requisicoes/cursoandroid/login/ui/login/MainActivity.java
@@ -0,0 +1,96 @@
+package com.requisicoes.cursoandroid.login.ui.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.requisicoes.cursoandroid.login.R;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_REQUEST_CODE = 7117; //Qualquer número que você quiser
+    List<AuthUI.IdpConfig> provedor;
+    Button btn_sign_out;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_sign_out = findViewById(R.id.btn_sign_out);
+        btn_sign_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Logout
+                AuthUI.getInstance()
+                        .signOut(MainActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                btn_sign_out.setEnabled(false);
+                                showSignInOptions();
+
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
+        //inicial provedor
+        provedor = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),    //Email Buider
+                new AuthUI.IdpConfig.PhoneBuilder().build(),    //Phone Builder
+            //    new AuthUI.IdpConfig.FacebookBuilder().build(), // Facebook Builder
+                new AuthUI.IdpConfig.GoogleBuilder().build()    //Google Builder
+        );
+
+        showSignInOptions();
+    }
+
+    private void showSignInOptions() {
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(provedor)
+                .setTheme(R.style.MyTheme)
+                .build(), MY_REQUEST_CODE
+        );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == MY_REQUEST_CODE){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if(resultCode == RESULT_OK){
+                //Pegar Usuário
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                //Exibir Toast do email
+                Toast.makeText(this, ""+user.getEmail(), Toast.LENGTH_SHORT).show();
+                //Configurar botão signout
+                btn_sign_out.setEnabled(true);
+            }
+            else{
+                Toast.makeText(this, ""+response.getError().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
